3c986e84ae250712245415e3cbf8853252ee7a19
paolocrosetto <paolo.crosetto@gmail.com>
2025-03-28 09:49:52 +0100

    Session 1 - Lab update

--- Data/Session 1 -- data analysis.R
+++ Data/Session 1 -- data analysis.R
@@ -3,34 +3,36 @@ library(tidyverse)
 library(broom)
 
 ## data import
-df <- read_csv("Data/Session_1_BDM_in_action_data.csv")
+df <- read_csv("Session_1_BDM_in_action_data.csv")
 
 ## let's run some tests!
 
-## test #1: is there an effect on period 1 of BEING OBSERVED in the future. 
-df %>% 
-  filter(period == 1) %>% 
-  group_by(product) %>% 
-  group_modify(~tidy(t.test(.$bid~.$control)))
-## NO EFFECT exactly as we thought. Good!
+## test #1: What is the initial WTP for the matched group, for each product? 
+##          for the isolated group? are they statistically different?
+##
+## predictions: 
 
-## test #2: is there convergence? and if so, towards high, middle, low
-## predictions: Vicktoria YES, upwards
-##              Ericka YAS, towards the middle
-df %>% 
-  filter(period <= 4) %>% 
-  filter(product == "chocolate") %>% 
-  group_by(period) %>% 
-  summarise(mean_bid = mean(bid), sd_bid = sd(bid))
+
+## test #2: Are there peer effects? 
+##          i.e.: does the average WTP move over periods 1-4 in treatment differently than in control? 
+##          is there convergence? and if so, towards high, middle, low
+##          check both the mean (to get direction) and st.dev. (to get convergence)
+##
+## predictions: 
+
 
 ## test #3: does giving AB organic info impact WTP? for chocolate? for wine? for both? 
-## predictions: positive impact; Giulia: higher for wine; 
-df %>% 
-  filter(period == 4 | period == 5) %>% 
-  group_by(product) %>% 
-  group_modify(~tidy(t.test(.$bid~.$period)))
+##          is the policy impact different for treatment and control?
+##
+## predictions: 
 
 
+## test #4: Are there peer effects AFTER the policy information shock?
+##          i.e.: does the average WTP move over periods 1-4 in treatment differently than in control? 
+##          is there convergence? and if so, towards high, middle, low
+##          check both the mean (to get direction) and st.dev. (to get convergence)
+##
+## predictions: 
 
 
 
